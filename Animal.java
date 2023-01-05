6e9dc02052c7b48fefc96e44b43f705f187cc7a9
wwbb <1804139910@qq.com>
2023-01-05 19:08:05 +0800

    多态的练习

--- Animal.java
+++ Animal.java
@@ -1,3 +1,9 @@
 public class Animal {
+    int age = 40;
+    int weight = 10;
+    public void eat()
+    {
+        System.out.println("动物吃东西");
+    }
 }
 
---
+++ Cat.java
@@ -0,0 +1,10 @@
+public class Cat extends Animal{
+    int age = 400;
+    int weight = 100;
+    @Override
+    public void eat()
+    {
+        System.out.println("猫吃东西");
+    }
+
+}
